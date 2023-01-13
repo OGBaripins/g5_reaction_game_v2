f62716a6421ff7c700601677640698d436a50390
Aleksandrs Barbins <bbarbins@gmail.com>
2023-01-14 01:41:08 +0200

    i very commit good messages

--- app/src/main/java/com/example/reaction_game/EntryActivity.java
+++ app/src/main/java/com/example/reaction_game/EntryActivity.java
@@ -13,34 +13,25 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
-        Button login_btn = (Button) findViewById(R.id.entry_login_btn);
-        Button register_btn = (Button) findViewById(R.id.entry_register_btn);
-        Button later_btn =  findViewById(R.id.entry_later_btn);
-        login_btn.setOnClickListener(v -> {
-            Intent myIntent = new Intent(this, LoginActivity.class);
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(myIntent);
-        });
-        register_btn.setOnClickListener(v -> {
-            Intent myIntent = new Intent(this, RegisterActivity.class);
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(myIntent);
-        });
-        later_btn.setOnClickListener(v -> {
-            Intent myIntent = new Intent(this, EntryActivity.class);
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(myIntent);
-        });
     }
 
     public void goToLogin(View v){
         Intent myIntent = new Intent(this, LoginActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(myIntent);
+    }
+
+    public void goToMainMenu(View v){
+        Intent myIntent = new Intent(this, MainActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(myIntent);
     }
 
     public void goToRegister(View v){
         Intent myIntent = new Intent(this, RegisterActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(myIntent);
+
     }
 
 }
--- app/src/main/java/com/example/reaction_game/LoginActivity.java
+++ app/src/main/java/com/example/reaction_game/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.reaction_game;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
-        ImageView back = ((ImageView) findViewById(R.id.login_back_btn));
-        back.setOnClickListener(v -> {
-            Intent myIntent = new Intent(this, EntryActivity.class);
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(myIntent);
-        });
+    public void goToEntry(View v){
+        Intent myIntent = new Intent(this, EntryActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(myIntent);
     }
 }
