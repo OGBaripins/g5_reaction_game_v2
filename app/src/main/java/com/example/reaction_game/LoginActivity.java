3386bb74e0fc9885b18b8f5ac6e6072df6d45cda
Aleksandrs Barbins <bbarbins@gmail.com>
2023-01-14 16:23:44 +0200

    Login and register functionality

--- app/src/main/java/com/example/reaction_game/LoginActivity.java
+++ app/src/main/java/com/example/reaction_game/LoginActivity.java
@@ -1,17 +1,55 @@
 package com.example.reaction_game;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button login_btn;
+    SharedPreferences sp;
+    String username_str, password_str;
+
+    @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.login_username_field);
+        password = findViewById(R.id.login_password_field);
+        login_btn = findViewById(R.id.login_submit_button);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(LoginActivity.this,"Click worked i guess", Toast.LENGTH_LONG).show();
+                username_str = username.getText().toString();
+                password_str = password.getText().toString();
+
+                if(sp.getString("username",null).equals(username_str)){
+
+                    if(sp.getString("password",null).equals(password_str)){
+                        Toast.makeText(LoginActivity.this,"Login Successfully", Toast.LENGTH_LONG).show();
+
+                        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(myIntent);
+                    }else{
+                        Toast.makeText(LoginActivity.this,"Incorrect Credentials2", Toast.LENGTH_LONG).show();
+                    }
+
+                }else{
+                    Toast.makeText(LoginActivity.this,"Incorrect Credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void goToEntry(View v){
--- app/src/main/java/com/example/reaction_game/RegisterActivity.java
+++ app/src/main/java/com/example/reaction_game/RegisterActivity.java
@@ -1,23 +1,98 @@
 package com.example.reaction_game;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    EditText email, username, password, password_rep;
+    Button register_btn;
+    SharedPreferences sp;
+    String email_str, username_str, password_str, password_rep_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.register_email_field); username = findViewById(R.id.register_username_field);
+        password = findViewById(R.id.register_password_field); password_rep = findViewById(R.id.register_password_field2);
+        register_btn = findViewById(R.id.login_submit_button);
+
+        sp = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email_str = email.getText().toString();
+                username_str = username.getText().toString();
+                password_str = password.getText().toString();
+                password_rep_str = password_rep.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                if(validateEmail(email_str)){
+                    editor.putString("email", email_str);
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Email entered incorrectly", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(username_str.length() >= 3){
+                    editor.putString("username", username_str);
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Username has to be at least 3 characters long", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(password_str.length() >= 8){
+                    editor.putString("password", password_str);
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Password has to be at least 8 characters long", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(password_rep_str.equals(password_str)){
+                    editor.putString("password_rep", password_rep_str);
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Password's have to match", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                editor.commit();
+                Toast.makeText(RegisterActivity.this,"Registered Successfully", Toast.LENGTH_LONG).show();
+
+                Intent myIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(myIntent);
+
+            }
+        });
     }
 
     public void goToEntry(View v){
         Intent myIntent = new Intent(this, EntryActivity.class);
         myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(myIntent);
+    }
+
+    public static boolean validateEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches() && email.length() <= 254;
     }
 }
