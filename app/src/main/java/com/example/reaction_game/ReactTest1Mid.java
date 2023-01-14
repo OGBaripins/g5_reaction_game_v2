65999a9be9038bb85eaaddae32c1439949c63990
Pukitis <rovsvg@gmail.com>
2023-01-14 21:45:39 +0200

    Finalised reaction test 1.

--- app/src/main/java/com/example/reaction_game/ReactTest1Mid.java
+++ app/src/main/java/com/example/reaction_game/ReactTest1Mid.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,7 +26,7 @@ public class ReactTest1Mid extends AppCompatActivity {
 
     public void goToResult(View v){
         resultTime = (System.currentTimeMillis() - startTime);
-        if (resultTime < resultBest || resultBest == 0){
+        if (resultTime < resultBest || resultBest == 0) {
             resultBest = resultTime;
         }
         results.add(resultTime);
--- app/src/main/java/com/example/reaction_game/ReactTest1PreActivity.java
+++ app/src/main/java/com/example/reaction_game/ReactTest1PreActivity.java
@@ -5,25 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class ReactTest1PreActivity extends AppCompatActivity {
+
+    TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_react_test1_pre);
+        description = (TextView) findViewById(R.id.reactDesc);
     }
 
     @SuppressLint("WrongConstant")
     public void timerFunction(View v){
         findViewById(R.id.bgPink).setClickable(false);
-        findViewById(R.id.reactDesc).setVisibility(-1);
+        description.setVisibility(-1);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < (new Random().nextInt((3 - 1) + 1) + 1); i++) {
+                    timer(1000);
+                    goToMid();
+                }
+            }
+        }, 1);
+        description.setVisibility(-1);
+    }
+
+    public void goToMid(){
+        Intent myIntent = new Intent(this, ReactTest1Mid.class);
+        startActivity(myIntent);
+    }
+
+    public void timer(int time){
         try {
-            Thread.sleep(new Random().nextInt((5000 - 2000) + 1) + 2000);
-            Intent myIntent = new Intent(this, ReactTest1Mid.class);
-            startActivity(myIntent);
+            Thread.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
