c11b280f9f80ce091cf2c1a1d26b9dce695c4ea9
Aleksandrs Barbins <bbarbins@gmail.com>
2023-03-28 01:19:29 +0300

    react tests scores -> firebase

--- app/src/main/java/com/example/reaction_game/testScreens/ReactTest1Activity.java
+++ app/src/main/java/com/example/reaction_game/testScreens/ReactTest1Activity.java
@@ -1,5 +1,8 @@
 package com.example.reaction_game.testScreens;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,10 +10,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.reaction_game.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ReactTest1Activity extends AppCompatActivity {
 
@@ -18,9 +34,10 @@ public class ReactTest1Activity extends AppCompatActivity {
     ImageView pinkBackground;
     static long startTime = 0;
     static double resultTime = 0;
-    static boolean clickedTooFast = true;
-    SharedPreferences sp, sp_user;
-    SharedPreferences.Editor editor;
+    static boolean clickedTooFast;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String, Object> return_data;
+    Map<String, Object> return_data1;
 
     @Override
     public void onBackPressed() {
@@ -32,8 +49,7 @@ public class ReactTest1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_react_test1_pre);
-        sp = getSharedPreferences("UserScores", Context.MODE_PRIVATE);
-        sp_user = getSharedPreferences("UserPrefs",Context.MODE_PRIVATE);
+        clickedTooFast = true;
         blueBackground = findViewById(R.id.bgBlue);
         pinkBackground = findViewById(R.id.bgPink);
         pinkBackground.setClickable(false);
@@ -64,18 +80,101 @@ public class ReactTest1Activity extends AppCompatActivity {
     public void goToResult(View v){
         clickedTooFast = false;
         resultTime = (System.currentTimeMillis() - startTime);
-        if(sp_user.getBoolean("isLoggedIn", false)){ // To not count scores if not logged in!!
-            editor = sp.edit();
-            editor.putInt("all_games_played", sp.getInt("all_games_played",0) + 1); // Take if you need to count all games played!!
-            editor.putInt("CH_games_played", sp.getInt("CH_games_played",0) + 1);
-            if (resultTime < sp.getFloat("CH_best_result",0) || sp.getFloat("CH_best_result",0) == 0) {
-                editor.putFloat("CH_best_result", (float)resultTime);
-            }
-            editor.putFloat("CH_result_sum", sp.getFloat("CH_result_sum",0) + (float)resultTime);
-            editor.putFloat("CH_result_average", (sp.getFloat("CH_result_sum",0) / sp.getInt("CH_games_played", 1)));
-            editor.commit();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){ // To not count scores if not logged in!!
+            String cur_user_email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+            assert cur_user_email != null;
+            general_games_played(cur_user_email);
+            db.collection(cur_user_email)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            Map<String, Object> user = new HashMap<>();
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if(!document.getId().equals("CH")){continue;}
+                                    Log.d(TAG, document.getId() + " => " + document.getData());
+                                    return_data = document.getData();
+                                    Log.d(TAG, return_data + " return data "+ return_data.isEmpty());
+
+                                    Log.d(TAG, "yes very " + Integer.parseInt(Objects.requireNonNull(return_data.get("CH_games_played")).toString()));
+                                    user.put("CH_games_played", Integer.parseInt(Objects.requireNonNull(return_data.get("CH_games_played")).toString()) + 1);
+                                    if (resultTime < Float.parseFloat(Objects.requireNonNull(return_data.get("CH_best_result")).toString()) || Float.parseFloat(Objects.requireNonNull(return_data.get("CH_best_result")).toString()) == 0) {
+                                        user.put("CH_best_result", (float)resultTime);
+                                    }else{
+                                        user.put("CH_best_result", Float.parseFloat(Objects.requireNonNull(return_data.get("CH_best_result")).toString()));
+                                    }
+                                    user.put("CH_result_sum", Float.parseFloat(Objects.requireNonNull(return_data.get("CH_result_sum")).toString())+(float)resultTime);
+                                    user.put("CH_result_average", Float.parseFloat(Objects.requireNonNull(return_data.get("CH_result_sum")).toString())/ Integer.parseInt(Objects.requireNonNull(return_data.get("CH_games_played")).toString()));
+                                }
+//                                Log.d(TAG, return_data + " return data (reactionTest1) "+ return_data.isEmpty());
+                                if(return_data == null){
+                                    Log.d(TAG, "Empty!!! ");
+                                    user.put("CH_games_played", 1);
+                                    user.put("CH_best_result", (float)resultTime);
+                                    user.put("CH_result_sum", (float)resultTime);
+                                    user.put("CH_result_average", (float)resultTime);
+                                }
+                                // Add a new document with a generated ID
+                                db.collection(cur_user_email).document("CH")
+                                        .set(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error writing document", e);
+                                            }
+                                        });
+                                Log.d(TAG, "Damn thats crazy"+ task.getResult().size());
+                            } else {
+                                Log.d(TAG, "Error getting documents.", task.getException());
+                            }
+                        }
+                    });
         }
         Intent myIntent = new Intent(this, ReactScoreActivity.class);
         startActivity(myIntent);
+    }
+
+    public void general_games_played(String cur_user_email){
+        db.collection(cur_user_email)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        Map<String, Object> userr = new HashMap<>();
+                        if (task.isSuccessful()) {
+                            if(task.getResult().size() == 0){
+                                userr.put("all_games_played", 1);
+                            }
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if(!document.getId().equals("GENERAL")){continue;}
+                                return_data1 = document.getData();
+                                userr.put("all_games_played", Integer.parseInt(Objects.requireNonNull(return_data1.get("all_games_played")).toString()) + 1);
+                            }
+                            db.collection(cur_user_email).document("GENERAL")
+                                    .set(userr)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                        }
+                                    });
+                        } else {
+                            Log.d(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
